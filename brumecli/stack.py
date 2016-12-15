d8e456815e1dc84aebe68194d2352038e662f339
Lou Ferrand <ferrand@ekino.com>
2016-12-15 11:57:44 +0100

    Remove colored events output

--- brumecli/stack.py
+++ brumecli/stack.py
@@ -1,7 +1,6 @@
 import time
 import os
 import boto3
-from color import Color
 from colors import red
 from botocore.exceptions import ClientError
 
@@ -155,7 +154,7 @@ class Stack():
     def _log_event(self, e):
         print('{:23s} {:36s} {:30s} {:30s} {}'.format(
             e['Timestamp'].strftime('%Y-%m-%d %H:%M:%S UTC'),
-            Color.for_status(e['ResourceStatus']),
+            e['ResourceStatus'],
             e['LogicalResourceId'],
             e['ResourceType'],
             e.get('ResourceStatusReason', ''),
